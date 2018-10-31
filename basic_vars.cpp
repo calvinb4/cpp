c5175e70bdb12e459d7d49b333d7f697ba621145
calvinb4 <calvinbertoncini@gmail.com>
2018-10-31 18:38:35 -0400

    second commit

---
+++ basic_vars.cpp
@@ -0,0 +1,15 @@
+/* Calvin Bertoncini
+ * Created 10/31/18
+ * 1.3 learncpp.com
+ */
+
+
+# include <iostream>
+
+int main()
+{
+  int x(42); 
+  std::cout << x;
+  std::cout << "\n";
+  return 0;
+}
