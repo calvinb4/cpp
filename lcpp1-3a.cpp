b9747fda7dc3532c8d17085a73f6d4e9321ef026
calvinb4 <calvinbertoncini@gmail.com>
2018-11-01 13:28:55 -0400

    fourth commit

---
+++ lcpp1-3a.cpp
@@ -0,0 +1,14 @@
+// Calvin Bertoncini
+// Created 11/1/18
+// learncpp.com 1.3a
+
+# include <iostream>
+
+int main()
+{
+  std::cout << "Enter a number: " << std::endl;
+  int x;
+  std::cin >> x;
+  std::cout << "You entered: " << x << std::endl;
+  return 0;
+}
